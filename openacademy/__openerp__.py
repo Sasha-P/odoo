7e32ece60a2b9049b7cb6ec96baf91cc02623e7c
Your Name <you@example.com>
2016-12-03 12:17:28 +0200

    fix tutorial bug

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -26,6 +26,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
