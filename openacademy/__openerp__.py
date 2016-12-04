77cd63173ca6badff184e0ad6f730862f07d38bf
Your Name <you@example.com>
2016-12-04 02:04:36 +0200

    Add a button to filter the courses for which the current user is the responsible in the course search view. Make it selected by default. Add a button to group courses by responsible user

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -18,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Test',
-    'version': '0.25',
+    'version': '0.26',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
